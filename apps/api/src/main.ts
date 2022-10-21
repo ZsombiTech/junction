ecf0bbe7d6ef069edb614ee83ab1281b8240cad3
Zsombor Horvath <horvathzsombor2005@gmail.com>
2022-10-21 09:07:18 +0200

    first commit

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { Message } from '@junction/api-interfaces';
 
 const app = express();
 
-const greeting: Message = { message: 'Welcome to api!' };
+const greeting: Message = { message: 'Welcome to Zsombi app!' };
 
 app.get('/api', (req, res) => {
   res.send(greeting);
--- apps/junction/src/app/app.tsx
+++ apps/junction/src/app/app.tsx
@@ -13,7 +13,7 @@ export const App = () => {
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to junction!</h1>
+        <h1>Welcome to HUNMARGYAR SZITYYAK!</h1>
         <img
           width="450"
           src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
