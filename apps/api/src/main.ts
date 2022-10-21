643170a36090ddaf2b30314deeb33647e4c29c73
Daniel Gergely <vargaking2005@gmail.com>
2022-10-21 21:15:07 +0200

    connected to mongodb
    you have to make a .env file with your credentials

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,25 @@
 import * as express from 'express';
 import { Message } from '@junction/api-interfaces';
+import { Db } from 'mongodb';
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 const greeting: Message = { message: 'Welcome to Zsombi app!' };
+
+main().catch(err => console.error(err));
+
+async function main() {
+  const DB_USER: string = process.env.DB_USER;
+  const DB_PASS: string = process.env.DB_PASS;
+
+  console.log(DB_USER);
+
+  await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@bytesgate.xdaa0yz.mongodb.net/?retryWrites=true&w=majority`);
+
+  console.log("Connected to MongoDB");
+}
 
 app.get('/api', (req, res) => {
   res.send(greeting);
